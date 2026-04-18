bf82cc25a1065ea527221da1936971fd0b1f9468
pranavdabane <pranavdabane41@gmail.com>
2026-04-18 18:51:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+let nav=document.querySelector(".navbar");
+window.onscroll=function(){
+    if(this.document.documentElement.scrollTop > 50){
+        nav.classList.add("header-scrolled");
+    }else{
+        nav.classList.add("header-scrolled");
+    }
+}
